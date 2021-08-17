e00898589b48f94de88173199f2ab5760ff746b4
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-08-17 15:22:41 +0100

    change to error handler

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const mongodb = 'mongodb+srv://soc-bootcamper:<bootcamp@cluster0.cws7p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongodb = 'mongodb+srv://soc-bootcamper:bootcamp@cluster0.cws7p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongodb, {useNewUrlParser:true, useUnifiedTopology:true});
 const db=mongoose.connection
-db.on('error', console.error("unable to connect"));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
