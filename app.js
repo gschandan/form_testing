dfcdcfa7d54e816df4e8e3564cd8bdf24fd06623
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-08-17 15:18:08 +0100

    connecting to dbase

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mongodb = 'mongodb+srv://soc-bootcamper:<bootcamp@cluster0.cws7p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongodb, {useNewUrlParser:true, useUnifiedTopology:true});
+const db=mongoose.connection
+db.on('error', console.error("unable to connect"));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
